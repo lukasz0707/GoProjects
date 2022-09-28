c65b265869fd86f8725eb4575228980429c6dc72
lukasz0707 <lukaszs0707@gmail.com>
2022-09-28 17:03:07 +0200

    Performant fibbonaci

---
+++ Numbers/fibonacci.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+// Performant big fibonacci n-th number sequential approach
+func fibonacci(n uint) *big.Int {
+	if n <= 1 {
+		return big.NewInt(int64(n))
+	}
+	var p, q = big.NewInt(0), big.NewInt(1)
+	for i := uint(1); i < n; i++ {
+		p.Add(p, q)
+		q, p = p, q
+	}
+
+	return q
+}
+
+// If you want the output to go into a file try:
+// go run fibonnaci.go > filename.txt
+func main() {
+	fmt.Println(fibonacci(5))
+}
