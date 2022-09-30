c3fe41942a11080a9b36eb7c159a431fc34dbf0f
lukasz0707 <lukaszs0707@gmail.com>
2022-09-30 13:03:25 +0200

    Improved performance

--- Numbers/is_prime.go
+++ Numbers/is_prime.go
@@ -7,10 +7,16 @@ import (
 
 // Check if number is prime
 func isPrime(n int) bool {
+	if n == 2 {
+		return true
+	}
 	if n < 2 {
 		return false
 	}
-	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+	if n%2 == 0 {
+		return false
+	}
+	for i := 3; i <= int(math.Sqrt(float64(n))); i += 2 {
 		if n%i == 0 {
 			return false
 		}
